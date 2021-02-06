dcb152db046543da70946d0bcbf9c033a470a379
Aleksandr Vikulin <52592231+debugger1979@users.noreply.github.com>
2021-02-06 18:20:11 +0800

    Пункт 1: Рекурсивный обход каталога

--- EXIF/exif_pars.py
+++ EXIF/exif_pars.py
@@ -3,7 +3,9 @@ import fileinput
 from exif import Image
 import fnmatch
 
-img_path = "D:/Distr/foto"
+img_path = "D:\\Distr\\foto"
 
-for root, dirs, files in os.walk(img_path):
-    print(root)
+for roots, dirs, files in os.walk(img_path):
+    for file in files:
+        
+        print(os.path.join(roots, file))
