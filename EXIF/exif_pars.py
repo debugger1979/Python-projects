f597f5ca0c618f5a8406b9152dfdb46790fc0a4f
Aleksandr Vikulin <52592231+debugger1979@users.noreply.github.com>
2021-02-06 21:36:28 +0800

    начало

--- EXIF/exif_pars.py
+++ EXIF/exif_pars.py
@@ -7,5 +7,9 @@ img_path = "D:\\Distr\\foto"
 
 for roots, dirs, files in os.walk(img_path):
     for file in files:
-        
-        print(os.path.join(roots, file))
+        full_name = os.path.join(roots, file)
+        with open(full_name, 'rb') as image_file:
+            my_image = Image(image_file)
+            if my_image.has_exif:
+                
+        print()
