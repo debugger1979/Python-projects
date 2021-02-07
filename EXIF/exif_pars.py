7e341f83561ea11b798c9fef670d5335c73a567b
debugger1979 <alex.debugger@yandex.ru>
2021-02-07 14:19:07 +0800

    Проба пера с Pillow

--- EXIF/exif_pars.py
+++ EXIF/exif_pars.py
@@ -1,17 +1,26 @@
 import os
 import fileinput
 import fnmatch
-from exif import Image
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 #img_path = "D:\\Distr\\foto"
 img_path = "F:\\foto"
 
+def get_exif(filename):
+    i = Image.open(filename)
+    info = i._getexif(a)
+    return {TAGS.get(tag): value for tag, value in info.items()}
+
 for roots, dirs, files in os.walk(img_path):
     for file in files:
         full_name = os.path.join(roots, file)
-        with open(full_name, 'rb') as image_file:
-            my_image = Image(image_file)
-            if my_image.has_exif:
-                print(full_name)
-                print()
+        if fnmatch.fnmatch(full_name, '*.jpg'):
+            print(get_exif(full_name))
+ #       with open(full_name, 'rb') as image_file:
+ #           my_image = Image(image_file)
+ #           if my_image.has_exif:
+ #               print(full_name)
+ #               print()
+
                 
