33cac79dad16c3cd8a9d4dd0f0feb76d422f27cc
debugger1979 <alex.debugger@yandex.ru>
2021-02-07 14:38:53 +0800

    Pillow1

--- EXIF/exif_pars.py
+++ EXIF/exif_pars.py
@@ -9,14 +9,15 @@ img_path = "F:\\foto"
 
 def get_exif(filename):
     i = Image.open(filename)
-    info = i._getexif(a)
+    
+    info = i._getexif()
     return {TAGS.get(tag): value for tag, value in info.items()}
 
 for roots, dirs, files in os.walk(img_path):
     for file in files:
         full_name = os.path.join(roots, file)
         if fnmatch.fnmatch(full_name, '*.jpg'):
-            print(get_exif(full_name))
+            print(full_name, get_exif(full_name))
  #       with open(full_name, 'rb') as image_file:
  #           my_image = Image(image_file)
  #           if my_image.has_exif:
